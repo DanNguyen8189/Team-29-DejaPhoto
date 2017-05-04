ea0be6797049ee13dcee43a32220a752cff364da
DanXar <dan001@ucsd.edu>
2017-05-03 17:01:59 -0700

    add class

---
+++ app/src/main/java/com/team29/cse110/team29dejaphoto/DisplayCycle.java
@@ -0,0 +1,158 @@
+package com.team29.cse110.team29dejaphoto;
+import java.util.*;
+
+/**
+ * Created by Dan Nguyen Brian Orensztein on 5/3/2017.
+ */
+
+public class DisplayCycle {
+    public class Priorities {
+
+        // Check which features of DejaVu Mode are on
+        boolean isLocationOn;
+        boolean isDateOn;
+        boolean isTimeOn;
+
+        PriorityQueue<DejaPhoto> album; // Holds a sorted list of photos based on priority
+
+        /* Priorities Default Constructor */
+        public Priorities() {
+            isLocationOn = true;
+            isDateOn = true;
+            isTimeOn = true;
+
+            album = new PriorityQueue<DejaPhoto>(); // *******Do we need to pass in the comparator???********
+        }
+
+    /* Updates the priorities
+    public void updatePriorities() {
+        TODO
+    }
+    */
+    }
+
+    public class History {
+
+        LinkedList<DejaPhoto>  historyData; // Holds the history objects
+        ListIterator<DejaPhoto> listIterator; // Holds current position in the history list
+        int maxTen; // Keep track of how many photos are in the history
+        int counter; // Keep track of which photo in history we are on
+
+        /* History Default Constructor */
+        public History() {
+            historyData = new LinkedList<DejaPhoto>();
+            listIterator = historyData.listIterator();
+            maxTen = 0;
+            counter = 0;
+        }
+
+        /**
+         * Used to check if we are currently at the latest DejaPhoto in the history list.
+         * param None
+         * return boolean - False if the number of photos in the list equal the counter.
+         *                   Otherwise, returns true.
+         */
+        public boolean checkValidNext() {
+            // No next photo in the history list
+            if (maxTen == counter) {
+                return false;
+            }
+
+            return true;
+
+        }
+
+        /***************************************** TODO
+         public boolean checkValidPrev() {
+
+         }
+         **********************************************/
+
+        /**
+         * Used to increment the counter (which photo we are looking at in the history
+         * list) and to get the next photo.
+         * param None
+         * return DejaPhoto - the photo to be displayed
+         */
+        public DejaPhoto getNext() {
+
+            counter++;
+            return listIterator.next();
+        }
+
+        /***************************************** TODO
+         public DejaPhoto getPrev() {
+
+         }
+         *********************************************/
+
+        /**
+         * Used to remove DejaPhoto's from the history list when it reaches 10
+         * photos, and to add DejaPhoto's to the history when we swipe left.
+         * @param photo - Photo to add from priority queue to history list
+         * @return DejaPhoto - Photo to be added back into the PQ if removed from
+         *                     the list
+         */
+        public DejaPhoto updateHistory(DejaPhoto photo) {
+            DejaPhoto toMove = null; // Holds the photo removed from the list
+
+            // List is at max capacity
+            if (maxTen == 10) {
+                // Remove the oldest photo from the list
+                toMove = historyData.pollFirst();
+                maxTen--;
+            }
+
+            // Add newest photo from PQ to the list
+            historyData.add(photo);
+            maxTen++;
+
+            return toMove;
+        }
+    }
+
+    // Create an instance of the History and Priorities class
+    History history = new History();
+    Priorities priorities = new Priorities();
+
+    boolean inHistory = false; // Used to check if we are in the history list or not
+
+    /**
+     * Used to get the next photo in the sequence, calling other helper methods to
+     * determine where to get the next photo.
+     * param none
+     * return DejaPhoto - photo to be displayed
+     */
+    public DejaPhoto getNextPhoto() {
+
+        DejaPhoto toDisplay = null; // Initialize the photo that will be displayed
+        DejaPhoto toAdd = null; // Iniitalize the photo to be added to the PQ album
+
+        // The photo we are looking at is not the latest photo in history
+        if ((history.checkValidNext() == true) && (inHistory == true)) {
+            toDisplay = history.getNext();
+            return toDisplay;
+        }
+
+        // We are no longer in the history list (now in PQ)
+        else {
+            inHistory = false;
+            toDisplay = priorities.album.poll();
+            toAdd = history.updateHistory(toDisplay);
+
+            // history list was at max capacity and a photo was removed to be re-added to PQ
+            if (toAdd != null) {
+                priorities.album.add(toAdd);
+            }
+        }
+
+        return toDisplay;
+    }
+
+    /************************************* TODO
+     public DejaPhoto getPrevPhoto() {
+
+     }
+     ***************************************/
+
+}
