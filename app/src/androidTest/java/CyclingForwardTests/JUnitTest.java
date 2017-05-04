8cc690f004568ed869cfa62ba96d49c9fa5471e5
Brian <Brian>
2017-05-04 16:56:48 -0700

    Add Cycle Forward test file

---
+++ app/src/androidTest/java/CyclingForwardTests/JUnitTest.java
@@ -0,0 +1,24 @@
+package CyclingForwardTests;
+
+import android.support.test.rule.ActivityTestRule;
+
+import com.team29.cse110.team29dejaphoto.DisplayCycle;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * Created by Brian Orensztein and Dan Nguyen on 5/3/2017.
+ */
+
+public class JUnitTest {
+
+    @Rule
+    //public ActivityTestRule<DisplayCycle> displayCycle = new ActivityTestRule<>(DisplayCycle.class);
+
+    @Test
+    public void testCheckValidNext() {
+
+
+    }
+}
