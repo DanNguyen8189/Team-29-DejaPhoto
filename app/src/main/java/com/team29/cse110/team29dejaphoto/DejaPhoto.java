de322e52791fab7e8bf8231a065237b1af7ba2e7
davidduplantier <dduplant@ucsd.edu>
2017-05-05 02:16:49 -0700

    Correct data types of DejaPhoto fields and implement calculateScore method.

--- app/src/main/java/com/team29/cse110/team29dejaphoto/DejaPhoto.java
+++ app/src/main/java/com/team29/cse110/team29dejaphoto/DejaPhoto.java
@@ -1,34 +1,43 @@
 package com.team29.cse110.team29dejaphoto;
 
+import android.net.Uri;
+
 /**
  * Created by David Duplantier and Noah Lovato on 5/1/17.
  */
 
 public class DejaPhoto implements Comparable<DejaPhoto> {
 
-    private String photoPath;     /* Path to photo */
-
-    private String latitude;      /* Lat and long coordinates where this photo was taken */
-    private String longitude;
-
-    private String time;          /* Time this photo was taken */
-
+    private Uri photoUri;         /* Uri for this photo */
+    private double latitude;      /* Lat and long coordinates where this photo was taken */
+    private double longitude;
+    private Long time;            /* Time this photo was taken (seconds since January 1st 1970 */
     private String date;          /* Day of the week this photo was taken */
+
+    // TODO There's are variety of other useful data like orientation, height, etc. that might become useful later on.
 
     private boolean karma;        /* Flags for karma, released, and whether the photo has been */
     private boolean released;     /* shown recently */
     private boolean showRecently;
 
-    // Score of this photo.
-    private int myScore;
+    private int myScore;          /* Priority score of this photo */
 
     /**
-     * DejaPhoto constructor.
+     * DejaPhoto constructor. The photo's data including Uri, latitude, longitude, time taken,
+     * and date taken, are passed as parameters to the constructor.
      */
-    public DejaPhoto() {
+    public DejaPhoto(Uri photoUri, double latitude, double longitude, Long time, String date) {
+
+        this.photoUri = photoUri;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.time = time;
+        this.date = date;
+
         karma = false;
         released = false;
         showRecently = false;
+
         myScore = 0;
     }
 
@@ -118,13 +127,48 @@ public class DejaPhoto implements Comparable<DejaPhoto> {
      * @param isLocationOn True to include location data in score calculation, and false otherwise.
      * @param isDateOn True to include date in score calculation, and false otherwise.
      * @param isTimeOn True to inlcude time in score calculation, and false otherwise.
-     * @return Integer value of this DejaPhoto object's priority score.
+     * @return int value of this DejaPhoto object's priority score.
      */
     public int calculateScore(boolean isLocationOn, boolean isDateOn, boolean isTimeOn) {
-        return 0;
+
+        int includeLocation = mapBooleanToInt(isLocationOn);
+        int includeDate = mapBooleanToInt(isDateOn);
+        int includeTime = mapBooleanToInt(isTimeOn);
+
+        return getKarmaPoints() +
+               includeLocation * getLocationPoints() +
+               includeDate * getDatePoints() +
+               includeTime * getTimeTakenPoints();
+
     }
 
 
     // Helper methods below
+
+    private int getKarmaPoints() {
+        return 0;
+    }
+
+    private int getLocationPoints() {
+        return 0;
+    }
+
+    private int getTimeTakenPoints() {
+        return 0;
+    }
+
+    private int getDatePoints() {
+        return 0;
+    }
+
+    /*
+     * To avoid a large, confusing if-else structure in the calculateScore method, simply
+     * use this method to map the boolean values to 0 (false) and 1 (true). Multiply these
+     * values with scores, so that multiplying by zero ignores a score, and 1 includes the
+     * score.
+     */
+    private int mapBooleanToInt(boolean value) {
+        return (value) ? 1 : 0;
+    }
 
 }
