ddda65177973b241521b65b16c979b63065f779f
davidduplantier <dduplant@ucsd.edu>
2017-05-28 15:40:48 -0700

    Skeleton for album gallery activities

---
+++ app/src/main/java/com/team29/cse110/team29dejaphoto/AlbumSelection.java
@@ -0,0 +1,13 @@
+package com.team29.cse110.team29dejaphoto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlbumSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_album_selection);
+    }
+}
