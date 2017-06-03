7c7c0b312443d21d937ed4742684f14410a35ccf
davidduplantier <dduplant@ucsd.edu>
2017-06-03 15:31:24 -0700

    Changes

--- app/src/main/java/com/team29/cse110/team29dejaphoto/models/User.java
+++ app/src/main/java/com/team29/cse110/team29dejaphoto/models/User.java
@@ -2,6 +2,9 @@ package com.team29.cse110.team29dejaphoto.models;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.team29.cse110.team29dejaphoto.interfaces.Photo;
+
+import java.util.ArrayList;
 
 /**
  * Created by David Duplantier on 6/3/17.
@@ -14,7 +17,7 @@ public class User {
 
 
     /*
-     * Constructor. Make connection to the realtime database.
+     * Constructor. Get reference to the Firebase realtime database.
      */
     public User() {
         database = FirebaseDatabase.getInstance();
@@ -22,6 +25,9 @@ public class User {
     }
 
     public String[] getFriends() {
+
+        ArrayList<Photo> friendsPhotos = new ArrayList<>();
+
         return null;
     }
 }
