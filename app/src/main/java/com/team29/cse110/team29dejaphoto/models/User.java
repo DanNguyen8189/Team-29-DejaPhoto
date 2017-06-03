2ef7afb631deb82673cecbbd9b267134f562e618
davidduplantier <dduplant@ucsd.edu>
2017-06-03 15:09:54 -0700

    Begin User class.

---
+++ app/src/main/java/com/team29/cse110/team29dejaphoto/models/User.java
@@ -0,0 +1,15 @@
+package com.team29.cse110.team29dejaphoto.models;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+/**
+ * Created by David Duplantier on 6/3/17.
+ */
+
+public class User {
+
+    /* Constructor. Make connection to the realtime database. */
+    public User() {
+
+    }
+}
